ad8d58ec694bc635cc9e53a84f43b320a6ad5c73
khemraj-dev <khemraj.47billion@gmail.com>
2018-10-27 14:25:30 +0530

    initial project

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { DataService } from '../data.service';
+
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss'],
+
+})
+export class UsersComponent implements OnInit {
+  users$: Object;
+
+  constructor(private data: DataService) { }
+
+  ngOnInit() {
+
+    this.data.getUsers().subscribe(data => {
+      console.log(data);
+      this.users$ = data
+    });
+  }
+
+}
